721c70f08424a277bbed710e744d677c05b98b62
Martyn Pitchford <martyn@pitchblend.co.uk>
2016-06-09 14:20:09 +0000

    Updated all files

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   
   def  save_with_payment
     if valid?
-      # Creates and charges customer via Stripe which returns a Customer ID
+      # Creates and charges customer via Stripe which returns a Customer object
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
