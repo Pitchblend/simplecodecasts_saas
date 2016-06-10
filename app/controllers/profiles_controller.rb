6329863a475fd9f9c9d7d91e5eeebde68d7d9041
Martyn Pitchford <martyn@pitchblend.co.uk>
2016-06-10 09:06:08 +0000

    Added profiles new action and corrected db migrate issue

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
 class ProfilesController < ApplicationController
-    
+    def new
+        # Form where user can fill out their own profile.
+        # Determine who is logged in.
+        @user = User.find( params[:user_id])
+        # Build form
+        @profile = @user.build_profile
+    end
 end
